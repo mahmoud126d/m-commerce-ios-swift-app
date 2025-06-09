62dff36381d7fb990da535f2e98e6208aceae8f0
NadiaFarid799 <nadianody825@gmail.com>
2025-06-10 02:27:41 +0400

    Update HomeRepositoryImpl.swift

--- Shoplet/Domain/Repository/HomeRepositoryImpl.swift
+++ Shoplet/Domain/Repository/HomeRepositoryImpl.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+class HomeRepositoryImpl: HomeRepository {
+    func getAllBrands(completion: @escaping (Result<[SmartCollection], Error>) -> Void) {
+           APIClient.getBrands { result in
+               switch result {
+               case .success(let response):
+                   if let brands = response.smart_collections {
+                       completion(.success(brands))
+                   } else {
+                       completion(.failure(NetworkError.invalidResponse))
+                   }
+               case .failure(let error):
+                   completion(.failure(error))
+               }
+           }
+       }
+
+       func getBestSellers(completion: @escaping (Result<[PopularProductItem], Error>) -> Void) {
+           APIClient.getPopularProducts { result in
+               switch result {
+               case .success(let response):
+                   if let products = response.products {
+                       completion(.success(products))
+                   } else {
+                       completion(.failure(NetworkError.invalidResponse))
+                   }
+               case .failure(let error):
+                   completion(.failure(error))
+               }
+           }
+       }
+}
