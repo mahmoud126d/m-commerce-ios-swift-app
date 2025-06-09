0f3ac68e5eb448781798377a9dea05e3cdefeb34
NadiaFarid799 <nadianody825@gmail.com>
2025-06-10 02:26:22 +0400

    Update GetAllBrandsUseCase.swift

--- Shoplet/Domain/UseCases/GetAllBrandsUseCase.swift
+++ Shoplet/Domain/UseCases/GetAllBrandsUseCase.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+final class GetAllBrandsUseCase {
+    private let repository: HomeRepository
+    
+    init(repository: HomeRepository) {
+        self.repository = repository
+    }
+
+    func execute(completion: @escaping (Result<[SmartCollection], Error>) -> Void) {
+        repository.getAllBrands(completion: completion)
+    }
+}
