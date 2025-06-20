f14a038f7ebdeb28508c8213348c4b728c2a6e63
mariam175 <mariamrafaat28@gmail.com>
2025-06-21 01:16:16 +0300

    currency

---
+++ Shoplet/Domain/Repository/CurrencyRepository.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyRepository.swift
+//  Shoplet
+//
+//  Created by Macos on 20/06/2025.
+//
+
+import Foundation
---
+++ Shoplet/Presentation/Features/Currency/View/CurrencyView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyView.swift
+//  Shoplet
+//
+//  Created by Macos on 20/06/2025.
+//
+
+import SwiftUI
+
+struct CurrencyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyView()
+}
