0e6df48825497d9952c8a627091852d9655d18f3
mariam175 <mariamrafaat28@gmail.com>
2025-06-23 02:52:30 +0300

    payment

---
+++ Shoplet/Presentation/Features/Cart/View/ApplePayButton.swift
@@ -0,0 +1,18 @@
+//
+//  ApplePayButton.swift
+//  Shoplet
+//
+//  Created by Macos on 22/06/2025.
+//
+
+import SwiftUI
+
+struct ApplePayButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApplePayButton()
+}
---
+++ Shoplet/Presentation/Features/Cart/ViewModel/ApplePay.swift
@@ -0,0 +1,8 @@
+//
+//  ApplePay.swift
+//  Shoplet
+//
+//  Created by Macos on 22/06/2025.
+//
+
+import Foundation
