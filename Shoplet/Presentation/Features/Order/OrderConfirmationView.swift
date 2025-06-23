95f5e6981c462fe3ffbfe9774aa5763190fc72dd
Nadia <nadianody825@gmail.com>
2025-06-23 18:24:47 +0400

    add confirmation page

--- Shoplet/Presentation/Features/Order/OrderConfirmationView.swift
+++ Shoplet/Presentation/Features/Order/OrderConfirmationView.swift
@@ -7,33 +7,7 @@
 
 import SwiftUI
 
-//struct OrderConfirmationView: View {
-//    var body: some View {
-//        VStack(spacing: 24) {
-//            Image(systemName: "checkmark.circle.fill")
-//                .resizable()
-//                .frame(width: 100, height: 100)
-//                .foregroundColor(.green)
-//
-//            Text("Order Confirmed!")
-//                .font(.title)
-//                .bold()
-//
-//            Text("Thank you for your purchase. A confirmation has been sent to your email.")
-//                .multilineTextAlignment(.center)
-//                .padding()
-//
-//            Button("Continue Shopping") {
-//                // You can pop to root view or navigate to home
-//            }
-//            .padding()
-//            .background(Color.primaryColor)
-//            .foregroundColor(.white)
-//            .cornerRadius(8)
-//        }
-//        .padding()
-//    }
-//}
+
 struct OrderConfirmationView: View {
     let totalPrice: String
     let customerName: String?
