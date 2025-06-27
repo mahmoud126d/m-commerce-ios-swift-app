f1423374a547baaa6a494fd68b314e630f4398b5
mariam175 <mariamrafaat28@gmail.com>
2025-06-27 17:32:35 +0300

    networkTesting

---
+++ ShopletTests/Network/MockAPIClient.swift
@@ -0,0 +1,8 @@
+//
+//  MockAPIClient.swift
+//  ShopletTests
+//
+//  Created by Macos on 27/06/2025.
+//
+
+import Foundation
---
+++ ShopletTests/Network/NetworkManagerTest.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManagerTest.swift
+//  ShopletTests
+//
+//  Created by Macos on 26/06/2025.
+//
+
+import Foundation
+
+class NetworkManagerTest{
+    
+}
