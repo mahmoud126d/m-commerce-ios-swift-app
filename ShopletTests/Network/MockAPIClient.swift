90298fd29d10bfa6babcd2c9f4df835c60e8eb3e
mariam175 <mariamrafaat28@gmail.com>
2025-06-27 17:32:51 +0300

    networkTesting

--- ShopletTests/Network/MockAPIClient.swift
+++ ShopletTests/Network/MockAPIClient.swift
@@ -6,3 +6,107 @@
 //
 
 import Foundation
+@testable import Shoplet
+
+class MockAPIClient : APIClientType{
+    static func getProducts(completion: @escaping (Result<Shoplet.ProductResponse, Shoplet.NetworkError>) -> Void) {
+        
+        let product = ProductModel(id: 1, title: "Mock Product", bodyHTML: nil, vendor: nil, productType: nil, tags: nil, status: nil, variants:nil, options: nil, images: nil, image: nil)
+        let productRespone = ProductResponse(products: [product])
+        completion(.success(productRespone))
+    }
+    
+    static func getProductsByIds(ids: [String], completion: @escaping (Result<Shoplet.ProductResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getPopularProducts(completion: @escaping (Result<Shoplet.ProductResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getBrands(completion: @escaping (Result<Shoplet.BrandsResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getPriceRules(completion: @escaping (Result<Shoplet.PriceRuleResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getPriceRulesById(priceRuleId: Int, completion: @escaping (Result<Shoplet.SinglePriceRuleResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getCoupons(id: Int, completion: @escaping (Result<Shoplet.CouponResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func createDraftOrder(draftOrder: Shoplet.DraftOrderItem, completion: @escaping (Result<Shoplet.DraftOrderItem, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func updateDraftOrder(draftOrder: Shoplet.DraftOrderItem, dtaftOrderId: Int, completion: @escaping (Result<Shoplet.DraftOrderItem, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getDraftOrderById(dtaftOrderId: Int, completion: @escaping (Result<Shoplet.DraftOrderItem, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getDraftOrders(completion: @escaping (Result<Shoplet.DraftOrdersResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func deleteDraftOrder(dtaftOrderId: Int, completion: @escaping (Result<Shoplet.EmptyResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func createCustomer(customer: Shoplet.CustomerRequest, completion: @escaping (Result<Shoplet.CustomerAuthResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getCustomerById(id: Int, completion: @escaping (Result<Shoplet.CustomerAuthResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getAllCustomers(completion: @escaping (Result<Shoplet.CustomerListResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func updateCustomer(customerId: Int, customer: Shoplet.CustomerRequest, completion: @escaping (Result<Shoplet.CustomerAuthResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func createAddress(address: Shoplet.AddressRequest, customerId: Int, completion: @escaping (Result<Shoplet.AddressRequest, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getUserAddresses(customerId: Int, completion: @escaping (Result<Shoplet.AddressResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func markAddresseDefault(customerId: Int, addressId: Int, completion: @escaping (Result<Shoplet.AddressRequest, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func deleteAddresse(customerId: Int, addressId: Int, completion: @escaping (Result<Shoplet.EmptyResponse, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getEgyptCities(completion: @escaping (Result<Shoplet.Cities, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func getCurrencies(completion: @escaping (Result<Shoplet.CurrencyExChange, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    static func completeOrder(draftOrderId: Int, completion: @escaping (Result<Shoplet.DraftOrderItem, Shoplet.NetworkError>) -> Void) {
+        
+    }
+    
+    func fetchAllOrders(completion: @escaping (Result<[Shoplet.ShopifyOrder], Error>) -> Void) {
+        
+    }
+    
+    
+}
--- ShopletTests/Network/MockAPIClientTest.swift
+++ ShopletTests/Network/MockAPIClientTest.swift
@@ -9,12 +9,13 @@ import XCTest
 
 final class MockAPIClientTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var mockAPI : MockAPIClient?
+    override func setUp() {
+        mockAPI = MockAPIClient()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown()  {
+       mockAPI = nil
     }
 
     func testExample() throws {
@@ -29,6 +30,17 @@ final class MockAPIClientTest: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+        }
+    }
+    func testMockGetProducts(){
+        MockAPIClient.getProducts { res in
+            switch res{
+            case .success(let res):
+             XCTAssertNotNil(res)
+            case .failure(_):
+                XCTFail()
+            }
+        
         }
     }
 
